d66c96e80fa24bee7812abbf877545051cfaa338
Emmanuel <115760511+EmmanuelBaldeon@users.noreply.github.com>
2023-10-17 19:41:21 -0300

    Update eliminar_empleado.php

--- eliminar_empleado.php
+++ eliminar_empleado.php
@@ -1,1 +1,12 @@
+<?php
 
+if (!empty($_GET['idEmpleado'])) {
+  $id= $_GET['idEmpleado'];
+  $sql=$conexion->query("DELETE FROM empleados WHERE idEmpleado=$id ");
+  if ($sql==1) {
+    echo '<div class="alert alert-success">Empleado elimnado correctamente</div>';
+  } else {
+    echo '<div class="alert alert-danger">Error al eliminar empleado</div>';
+  }
+}  
+?>
