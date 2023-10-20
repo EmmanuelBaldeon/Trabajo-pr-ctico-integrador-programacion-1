9c397ca540a84178322cd7a15017dc9d7e874d91
damian-trucco <111621930+damian-trucco@users.noreply.github.com>
2023-10-19 23:13:25 -0300

    Update login.php

--- login.php
+++ login.php
@@ -10,27 +10,35 @@
 <body>
   <h1 class="text-center p-3">Sistema de Gestion de Recursos Humanos</h1>
   <h2 class="text-center p-3">Login</h2>
-  
-  <?php
-    include "modelo/conexion.php";
-    include "controlador/controlador.php";
-  ?>
-
   <div class= "container">
     <div class="login-content">
       <form method="POST" class="align-center" action="">
+  <?php
+         include "modelo/conexion.php";
+         include "controlador/controlador.php";
+  ?>
+  <div class="input-div one">
+    <div class="i">
+      <i class="fas fa user"></i>
+    </div>
         <div class="mb-3">
           <label class="form-label">Usuario</label>
-          <input id="nombreUsuario" class="input" type="text" name="nombreUsuario">
+          <input id="usuario" class="input" type="text" name="usuario">
         </div>
+  </div>
+  <div class="input-div pass">
+    <div class="i">
+      <i class="fas fa user"></i>
+    </div>
         <div class="mb-3">
           <label class="form-label">Contraseña</label>
           <input id="clave" class="input" type="password" name="clave">
         </div>
+  </div>
         <div class="view">
-          <div class="fas fa-eye verPassword" onclick="vista()" id="verPassword"></div>
+          <div class="fas fa-eye verClave" onclick="vista()" id="verClave"></div>
         </div>
-        <button name="btningresar" type="submit" class="btn btn-primary">Iniciar sesion</button>
+        <input name="btnIngresar" class="btn btn-primary" type="submit" value="Iniciar sesion">
       </form>
 
   </body>
