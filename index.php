2e4fa992d231929cb9a86a1db3c1091152ab8379
damian-trucco <111621930+damian-trucco@users.noreply.github.com>
2023-10-19 23:14:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
     
     <div class="container-fluid row">
         <form class="col 4 p-3" method="POST">
-          <h3 class="text-center ext-secondary">Ingreso de Datos</h3>
+          <h3 class="text-center alert alert-secondary">Ingreso de Datos</h3>
 
           <div class="mb-3">
             <label class="form-label">Nombre</label>
@@ -115,9 +115,10 @@
               <?php }
 
              ?>
+               
              </tbody>
            </table>
         </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
-</html>
+  </html>
