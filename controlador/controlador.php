55821a1c38c6b13743b49983b8357a45f96a3beb
damian-trucco <111621930+damian-trucco@users.noreply.github.com>
2023-10-19 23:15:55 -0300

    Update controlador.php

--- controlador/controlador.php
+++ controlador/controlador.php
@@ -1,18 +1,21 @@
-<?php
+<?php 
 
-if (!empty($_POST['btningresar'])) {
-  if (!empty($_POST['nombreUsuario']) && !empty($_POST['clave'])) {
-    echo '<div class="alert alert-danger">Los campos no pueden estar vacios</div>';
-  } else {	
-    $nombreUsuario = $_POST['nombreUsuario'];
-    $clave = $_POST['clave'];
-    $sql = "SELECT * FROM usuarios WHERE usuario = '$nombreUsuario' && password = '$clave'";
-    if ($datos=$sql->fetch_object()) {
-      header("location:index.php"); 
+if (!empty($_POST["btnIngresar"])) {
+    if (empty($_POST["usuario"]) && empty($_POST["clave"])) {
+        echo '<div class="alert alert-danger">Los campos están vacíos</div>';
     } else {
-      echo '<div class="alert alert-danger">Acceso denegado</div>';
+        $usuario=$_POST["usuario"];
+        $clave=$_POST["clave"];
+
+        $sql=$conexion->query(" SELECT * FROM Usuarios WHERE nombre_usuario = '$usuario' && clave= '$clave' ");
+
+        if ($datos=$sql->fetch_object()) {
+            header("Location: index.php");
+        } else {
+            echo '<div class="alert alert-danger">Usuario y/o clave incorrecto/s</div>';
+        }
+       
     }
-  }
 }
 
 ?>
